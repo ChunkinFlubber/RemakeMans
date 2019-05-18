76b197ff7238e7442f2005c33ebe9f239a5a9527
ChunkinFlubber <ChunkinFlubber@users.noreply.github.com>
2019-05-17 20:41:08 -0500

    Added Jump Height Mutation

---
+++ Assets/Scripts/Mutations/JumpHeightMutation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Mutations/JumpHeightMutation", fileName = "JumpHeightMutation")]
+public class JumpHeightMutation : Mutation
+{
+	[SerializeField]
+	private float HeightPerStack = 0.25f;
+
+	private CharacterMovementSettings MoveSettings = null;
+	public override void Init(MutationSystem master)
+	{
+		base.Init(master);
+		MoveSettings = Master.MoveSystem.MovementData;
+		MoveSettings.JumpHeightMutated = MoveSettings.BaseJumpHeight + Stack * HeightPerStack;
+	}
+
+	public override void AddStack()
+	{
+		base.AddStack();
+		MoveSettings.JumpHeightMutated = MoveSettings.BaseJumpHeight + Stack * HeightPerStack;
+	}
+
+	public override void RemoveStack()
+	{
+		base.RemoveStack();
+		MoveSettings.JumpHeightMutated = MoveSettings.BaseJumpHeight + Stack * HeightPerStack;
+	}
+
+	public override void Destroy()
+	{
+		base.Destroy();
+
+	}
+}
