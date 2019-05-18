2544e3bcf890b65012328c3dc714dc116a413493
ChunkinFlubber <ChunkinFlubber@users.noreply.github.com>
2019-05-17 20:41:46 -0500

    Added a billborading script for objects

---
+++ Assets/Scripts/BillboardObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class BillboardObject : MonoBehaviour
+{
+	Transform PlayerTransform = null;
+	private void Start()
+	{
+		PlayerTransform = FindObjectOfType<Character>().transform;
+	}
+	void FixedUpdate()
+    {
+        if(PlayerTransform)
+		{
+			transform.LookAt(PlayerTransform);
+		}
+    }
+}
