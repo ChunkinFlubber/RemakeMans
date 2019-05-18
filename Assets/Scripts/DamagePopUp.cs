a44c9eecf07e10beefa632a4788c3d48110a9e3f
ChunkinFlubber <ChunkinFlubber@users.noreply.github.com>
2019-05-17 20:43:43 -0500

    Started the DamagePopUp UI in world space might change it to screen space later

---
+++ Assets/Scripts/DamagePopUp.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class DamagePopUp : MonoBehaviour
+{
+	[SerializeField]
+	TextMeshPro TextMesh = null;
+
+	Vector3 Velocity = Vector3.zero;
+	float Gravity = -10.0f;
+	float MaxTimeOnScreen = 1.25f;
+	float CurrentTimeOnScreen = 0.0f;
+
+	void OnEnable()
+    {
+		transform.localScale = new Vector3(1, 1, 1);
+		Velocity = new Vector3(Random.Range(-1, 1), Random.Range(1, 3), Random.Range(-1, 1)).normalized;
+    }
+
+    void FixedUpdate()
+    {
+		Velocity.y += Gravity * Time.deltaTime;
+
+		float percentTime = CurrentTimeOnScreen / MaxTimeOnScreen;
+		transform.localScale = new Vector3(percentTime,percentTime,percentTime);
+
+		CurrentTimeOnScreen += Time.deltaTime;
+		if(CurrentTimeOnScreen >= MaxTimeOnScreen)
+		{
+			//ReturnMe();
+		}
+    }
+}
