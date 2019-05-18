d64528472f1f8329921c2d68a866df6b8a78afb5
ChunkinFlubber <ChunkinFlubber@users.noreply.github.com>
2019-05-17 22:43:34 -0500

    Pop up damage UI is in need tweeking and Art

--- Assets/Scripts/DamagePopUp.cs
+++ Assets/Scripts/DamagePopUp.cs
@@ -6,28 +6,60 @@ public class DamagePopUp : MonoBehaviour
 	[SerializeField]
 	TextMeshPro TextMesh = null;
 
+	DamagePopUpPool Master = null;
+
 	Vector3 Velocity = Vector3.zero;
 	float Gravity = -10.0f;
-	float MaxTimeOnScreen = 1.25f;
+	float MaxTimeOnScreen = 1.15f;
 	float CurrentTimeOnScreen = 0.0f;
 
-	void OnEnable()
-    {
+	public void Init(DamagePopUpPool master)
+	{
+		Master = master;
+	}
+
+	public void SetDamage(bool isCrit = false, int amount = 10, Color? color = null)
+	{
+		gameObject.SetActive(true);
+
+		TextMesh.text = amount.ToString();
+		if (!isCrit)
+		{
+			TextMesh.color = color ?? Color.white;
+			TextMesh.outlineWidth = 0.0f;
+		}
+		else
+		{
+			TextMesh.color = color ?? Color.yellow;
+			TextMesh.outlineColor = Color.magenta;
+			TextMesh.outlineWidth = 0.1f;
+		}
+
+		CurrentTimeOnScreen = 0.0f;
 		transform.localScale = new Vector3(1, 1, 1);
-		Velocity = new Vector3(Random.Range(-1, 1), Random.Range(1, 3), Random.Range(-1, 1)).normalized;
-    }
+		Velocity = new Vector3(Random.Range(-3, 3), Random.Range(4, 6), Random.Range(2, 4));
+	}
 
     void FixedUpdate()
     {
+		CurrentTimeOnScreen += Time.deltaTime;
+
 		Velocity.y += Gravity * Time.deltaTime;
 
-		float percentTime = CurrentTimeOnScreen / MaxTimeOnScreen;
+		float percentTime = 1.0f - (CurrentTimeOnScreen / MaxTimeOnScreen);
 		transform.localScale = new Vector3(percentTime,percentTime,percentTime);
 
-		CurrentTimeOnScreen += Time.deltaTime;
+		transform.Translate(Velocity * Time.deltaTime);
+
 		if(CurrentTimeOnScreen >= MaxTimeOnScreen)
 		{
-			//ReturnMe();
+			ReturnMe();
 		}
     }
+
+	public void ReturnMe()
+	{
+		Master.Return(this);
+		gameObject.SetActive(false);
+	}
 }
