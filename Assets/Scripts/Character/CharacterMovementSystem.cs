e0efae995940c1da6164c7aa765570e7f885c165
ChunkinFlubber <ChunkinFlubber@users.noreply.github.com>
2019-05-18 21:07:18 -0500

    Clean up

--- Assets/Scripts/Character/CharacterMovementSystem.cs
+++ Assets/Scripts/Character/CharacterMovementSystem.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using UnityEngine.Experimental.Input;
 
+[RequireComponent(typeof(CharacterController))]
 public class CharacterMovementSystem : MonoBehaviour
 {
     [SerializeField]
